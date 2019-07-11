92db939d8f80fdcc2d44c1562d75a6d6f11cd39b
Gustav Gränsbo <gusgr644@student.liu.se>
2019-07-11 13:07:10 +0200

    Added test of inner dimension broadcasting

--- tests/test_tensor_add.py
+++ tests/test_tensor_add.py
@@ -47,9 +47,24 @@ class TestTensorAdd(unittest.TestCase):
         assert t1.grad.tolist() == [[1., 1., 1.], [1., 1., 1.]]
         # The gradient of t2 should be doubled since all it's values are broadcasted to two places.
         assert t2.grad.tolist() == [[2., 2., 2.]]
+    
+    def test_broadcasted_add3(self) -> None:
+        """ In this test t2 has shape (3, 1, 2) and is broadcasted across an inner dimension"""
+        t1 = Tensor(np.ones(shape=(3,2,2)), requires_grad=True)
+        t2 = Tensor(np.ones(shape=(3,1,2)), requires_grad=True)
+
+        t3 = _add(t1, t2)
+
+        np.testing.assert_equal(t3.data, 2 * np.ones(shape=(3,2,2)))
+
+        t3.backward(np.ones(shape=(3,2,2)))
+
+        np.testing.assert_equal(t1.grad, np.ones(shape=(3,2,2)))
+        # The gradient of t2 should be doubled since all it's values are broadcasted to two places.
+        np.testing.assert_equal(t2.grad, 2 * np.ones(shape=(3,1,2)))
 
     def test_broadcasted_scalar_add(self) -> None:
-        """ In this test t2 is broadcasted only by repeating it's values"""
+        """ In this test t2 is a scalar"""
         t1 = Tensor([[1, 2, 3], [4, 5, 6]], requires_grad=True)
         t2 = Tensor(1, requires_grad=True)
 
