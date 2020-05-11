7181a926dad076cc77abfd19a4f9b8ba713a327b
Gustav Gränsbo <gusgr644@student.liu.se>
2020-05-11 22:39:29 +0200

    Implement SGD optimizer

---
+++ autograd/optim.py
@@ -0,0 +1,14 @@
+" Optimizers for autograd Modules "
+
+from autograd.module import Module
+
+class SGD:
+    " Stochastic Gradient Descent Optimizer"
+
+    def __init__(self, lr = 0.001) -> None:
+        self.lr = lr
+
+    def step(self, module: Module) -> None:
+        for parameter in module.parameters():
+            # FIXME: Figure out typing of inherited __isub__
+            parameter -= parameter.grad * self.lr
