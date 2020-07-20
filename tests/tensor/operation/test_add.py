5ae565782cea7d86be64762a6a949488f1c5dd46
Gustav Gränsbo <gustav.gransbo@gmail.com>
2020-07-20 22:45:45 +0200

    Minor clean up to Tensor addition tests

--- tests/tensor/operation/test_add.py
+++ tests/tensor/operation/test_add.py
@@ -4,19 +4,38 @@ from gustavgrad import Tensor
 
 
 class TestTensorAdd:
-    def test_simple_add(self) -> None:
+    def test_simple_add_result(self) -> None:
         t1 = Tensor([1, 2, 3], requires_grad=True)
         t2 = Tensor([4, 5, 6], requires_grad=True)
-
         t3 = t1 + t2
 
         assert t3.data.tolist() == [5, 7, 9]
         assert t3.requires_grad
 
+    def test_simple_add_backward(self) -> None:
+        t1 = Tensor([1, 2, 3], requires_grad=True)
+        t2 = Tensor([4, 5, 6], requires_grad=True)
+        t3 = t1 + t2
         t3.backward(np.asarray([1.0, 1.0, 1.0]))
 
         assert t1.grad.tolist() == [1.0, 1.0, 1.0]
         assert t2.grad.tolist() == [1.0, 1.0, 1.0]
+
+    def test_add_depends_on_requires_grad_true(self) -> None:
+        t1 = Tensor([1, 2, 3], requires_grad=True)
+        t2 = Tensor([4, 5, 6], requires_grad=True)
+        t3 = t1 + t2
+
+        assert t1 in [dependency.tensor for dependency in t3.depends_on]
+        assert t2 in [dependency.tensor for dependency in t3.depends_on]
+
+    def test_add_depends_on_requires_grad_false(self) -> None:
+        t1 = Tensor([1, 2, 3], requires_grad=True)
+        t2 = Tensor([4, 5, 6], requires_grad=False)
+        t3 = t1 + t2
+
+        assert t1 in [dependency.tensor for dependency in t3.depends_on]
+        assert t2 not in [dependency.tensor for dependency in t3.depends_on]
 
     def test_simple_add_no_grad(self) -> None:
         t1 = Tensor([1, 2, 3])
@@ -27,35 +46,42 @@ class TestTensorAdd:
         assert t3.data.tolist() == [5, 7, 9]
         assert not t3.requires_grad
 
-    def test_scalar_add(self) -> None:
+    def test_scalar_add_result(self) -> None:
         t1 = Tensor([1, 2, 3], requires_grad=True)
-
         t2 = t1 + 1
         assert t2.data.tolist() == [2, 3, 4]
 
+    def test_scalar_add_backward(self) -> None:
+        t1 = Tensor([1, 2, 3], requires_grad=True)
+        t2 = t1 + 1
         t2.backward(np.asarray([1.0, 1.0, 1.0]))
         assert t1.grad.tolist() == [1.0, 1.0, 1.0]
 
-    def test_array_add(self) -> None:
+    def test_array_add_result(self) -> None:
         t1 = Tensor([1, 2, 3], requires_grad=True)
-
         t2 = t1 + np.array([4, 5, 6])
         assert t2.data.tolist() == [5, 7, 9]
 
+    def test_array_add_backward(self) -> None:
+        t1 = Tensor([1, 2, 3], requires_grad=True)
+        t2 = t1 + np.array([4, 5, 6])
         t2.backward(np.asarray([1.0, 1.0, 1.0]))
         assert t1.grad.tolist() == [1.0, 1.0, 1.0]
 
-    def test_scalar_radd(self) -> None:
+    def test_scalar_radd_result(self) -> None:
         t1 = Tensor([1, 2, 3], requires_grad=True)
 
         t2 = 1 + t1
         assert type(t2) is Tensor
         assert t2.data.tolist() == [2, 3, 4]
 
+    def test_scalar_radd_backward(self) -> None:
+        t1 = Tensor([1, 2, 3], requires_grad=True)
+        t2 = 1 + t1
         t2.backward(np.asarray([1.0, 1.0, 1.0]))
         assert t1.grad.tolist() == [1.0, 1.0, 1.0]
 
-    def test_broadcasted_add1(self) -> None:
+    def test_broadcasted_add_extra_dimension(self) -> None:
         """ In this test t2 is broadcasted by adding a dimension and then
         repeating it's values"""
         t1 = Tensor([[1, 2, 3], [4, 5, 6]], requires_grad=True)
@@ -72,7 +98,7 @@ class TestTensorAdd:
         # broadcasted to two places.
         assert t2.grad.tolist() == [2.0, 2.0, 2.0]
 
-    def test_broadcasted_add2(self) -> None:
+    def test_broadcasted_add_same_dimension(self) -> None:
         """ In this test t2 is broadcasted only by repeating it's values"""
         t1 = Tensor([[1, 2, 3], [4, 5, 6]], requires_grad=True)
         t2 = Tensor([[1, 2, 3]], requires_grad=True)
@@ -88,7 +114,7 @@ class TestTensorAdd:
         # broadcasted to two places.
         assert t2.grad.tolist() == [[2.0, 2.0, 2.0]]
 
-    def test_broadcasted_add3(self) -> None:
+    def test_broadcasted_add_inner_dimension(self) -> None:
         """ In this test t2 has shape (3, 1, 2) and is broadcasted across an
         inner dimension"""
         t1 = Tensor(np.ones(shape=(3, 2, 2)), requires_grad=True)
