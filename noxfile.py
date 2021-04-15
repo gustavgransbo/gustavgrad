3c4d84a41ddcaaa672626d6621c6910844095a57
Gustav Gränsbo <gustav.gransbo@gmail.com>
2021-04-15 22:35:59 +0200

    Manage poetry in nox sessions using nox-poetry
    nox-poetry provides a drop in replacement for the nox Session object,
    which will install packages in a way that respects versions pinned by
    poetry.

--- noxfile.py
+++ noxfile.py
@@ -1,36 +1,39 @@
 import nox
+import nox_poetry
 
 nox.options.reuse_existing_virtualenvs = True
 nox.options.sessions = ["tests", "mypy", "lint"]
 
 
-@nox.session(python=["3.7", "3.8"])
+@nox_poetry.session(python=["3.7", "3.8"])
 def tests(session):
     """ Run test suit."""
     args = session.posargs or ["--cov"]
-    session.run("poetry", "install", external=True)
+    session.install("pytest", "pytest-cov", ".")
     session.run("pytest", *args)
 
 
-@nox.session(python="3.8")
+@nox_poetry.session(python="3.8")
 def lint(session):
     """ Lint using flake8."""
     args = session.posargs or []
-    session.run("poetry", "install", external=True)
+    session.install(
+        "flake8", "flake8-bugbear", "flake8-black", "flake8-isort",
+    )
     session.run("flake8", *args)
 
 
-@nox.session(python="3.8")
+@nox_poetry.session(python="3.8")
 def mypy(session):
     """ Type-check using mypy."""
     args = session.posargs or ["src"]
-    session.run("poetry", "install", external=True)
+    session.install("mypy", ".")
     session.run("mypy", *args)
 
 
-@nox.session(python="3.8")
+@nox_poetry.session(python="3.8")
 def codecov(session):
     """ Upload coverage data to Codecov"""
-    session.run("poetry", "install", external=True)
+    session.install("codecov", "coverage")
     session.run("coverage", "xml")
     session.run("codecov", *session.posargs)
